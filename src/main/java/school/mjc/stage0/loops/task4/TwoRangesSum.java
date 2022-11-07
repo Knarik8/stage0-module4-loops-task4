6f1cb7462894f0d7fff34a49aaffec8a27a06a18
Armine8 <armisha15@mail.ru>
2022-11-07 19:46:53 +0300

    Task #4. Loop Keywords

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
-    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+    public static void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+        }
+        if (lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        }
+        if (numberToSkip <= lastInRow && lastInRow > 0) {
+            int skippedSum = 0;
+            int coutedSum = 0;
+            for (int i = 1; i <= lastInRow; i++) {
+                if (i <= numberToSkip) {
+                    skippedSum += i;
+                } else if (i <= lastInRow) {
+                    coutedSum += i;
+                }
+            }
+            System.out.println("skipped sum is " + skippedSum);
+            System.out.println("counted sum is " + coutedSum);
 
+        }
+    }
+    public static void main(String[] args){
+        printSumOfTwoRanges(15,10);
     }
 }
